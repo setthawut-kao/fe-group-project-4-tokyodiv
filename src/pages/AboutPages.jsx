1d7e962042c13851b0db742882f7c94f0327751f
thunyaluk <thunyalukluecha18@gmail.com>
2025-09-09 16:00:48 +0700

    feat(about-us-page):create profile

--- src/pages/AboutPages.jsx
+++ src/pages/AboutPages.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 const AboutPages = () => {
   return (
-    <div>AboutPages</div>
+    <Card className="w-full max-w-sm">
+  <CardHeader>
+    <CardTitle>Name</CardTitle>
+  </CardHeader>
+</Card>
+
   )
 }
 
