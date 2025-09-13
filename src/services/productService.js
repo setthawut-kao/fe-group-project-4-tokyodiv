25cad3d275703cb1fb0bfd746103993a29ed9a82
setthawut-kao <setthawut.kao@gmail.com>
2025-09-14 00:25:34 +0700

    Fixed(JS):edit cate:productService

--- src/services/productService.js
+++ src/services/productService.js
@@ -2,9 +2,20 @@ import api from "@/lib/axios";
 
 export const fetchProducts = async (params = {}) => {
   try {
-    const queryParams = new URLSearchParams(params).toString();
-    const response = await api.get(`/api/products?${queryParams}`);
+    let response;
+    const queryParams = new URLSearchParams(params);
 
+    if (queryParams.has("category")) {
+      const category = queryParams.get("category");
+      queryParams.delete("category");
+      const remainingParams = queryParams.toString();
+      response = await api.get(
+        `/api/products/category/${category}?${remainingParams}`
+      );
+    } else {
+      const allParams = queryParams.toString();
+      response = await api.get(`/api/products?${allParams}`);
+    }
     return response.data;
   } catch (error) {
     console.error("Error fetching products:", error);
