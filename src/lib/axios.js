112babf344f6e748da068ee5c5b43a833b0c595c
setthawut-kao <setthawut.kao@gmail.com>
2025-09-16 11:23:24 +0700

    fixed(err): axios&vercel

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,13 +1,8 @@
 import axios from "axios";
 
-// pick VITE_API_URL in dev, VITE_PUBLIC_API_URL in prod
-const baseURL = import.meta.env.DEV
-  ? import.meta.env.VITE_API_URL
-  : import.meta.env.VITE_PUBLIC_API_URL;
-
 const api = axios.create({
-  baseURL: baseURL || "http://localhost:8080",
-  withCredentials: true, // httpOnly cookie
+  baseURL: "/api",
+  withCredentials: true,
 });
 
 export default api;
